a092fef6fa6b98f75049777886bc033f5fa7c841
samyakkumarsingh561-cell <samyakkumarsingh561@gmail.com>
2026-02-15 23:45:48 +0530

    Initial commit

---
+++ OOPSBannerAPP.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Ankush
+ * @version 1.0
+ */
+
+public class OOPSBannerAPP {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+
+}
---
+++ OOPSBannerAPP1.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Ankush
+ * @version 2.0
+ */
+
+public class OOPSBannerAPP1 {   // renamed to match the filename
+
+    public static void main(String[] args) {
+        System.out.println( "***** " + " " + " ***** " + " " +" ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ****");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "         **");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** **");
+        System.out.println(" ***** " + " " + " ***** " + " " + " ** " + " " + "   ******* ");
+    }
+
+}
